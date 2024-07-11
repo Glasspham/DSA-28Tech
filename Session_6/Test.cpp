3b638755ba61bd01d1249e2afc33b442fb2933b9
Glasspham <95564442+Glasspham@users.noreply.github.com>
2024-07-11 22:39:20 +0700

    Create Test.cpp
    [Sắp xếp - Tìm Kiếm]. Bài 1. Các số khác nhau trong mảng 
    Cho mảng số nguyên gồm n phần tử. Hãy đếm xem mảng có bao nhiêu phần tử 
    riêng biệt 
    Input Format 
    Dòng đầu Ɵên là N Dòng thứ 2 là các phần tử trong mảng a1, a2, ... an 
    Constraints 
    1≤n≤2.105; 1≤ai≤109 
    Output Format 
    In ra số lượng phần tử riêng biệt trong mảng.

---
+++ Session_6/Test.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n = 10, i = 5;
+    cout << i + n / 2;
+    return 0;
+}
