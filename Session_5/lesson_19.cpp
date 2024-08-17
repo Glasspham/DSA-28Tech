3143098157dd6b154b81d7182ae846f453513482
Glasspham <hathimongthu1980@gmail.com>
2024-08-17 14:48:08 +0700

    fix

--- Session_5/lesson_19.cpp
+++ Session_5/lesson_19.cpp
@@ -6,7 +6,7 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int n, m; cin >> n >> m;
-    int a[n][m];
+
     for(int i = 0; i < n; ++i)
         for(int j = 0; j < m; ++j)
             cin >> a[i][j];
